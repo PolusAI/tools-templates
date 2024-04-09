2e5a8b27586b45fbd119f0e02ed7d23e8a47fee9
hamshkhawar <hamdahshafqat@hotmail.com>
2024-04-09 14:09:49 -0500

    remove example repo

--- tabular-templates/python-template/awesome-function-tool/src/polus/tabular/package1/package2/awesome_function/__init__.py
+++
@@ -1,7 +0,0 @@
-"""awesome_function."""
-
-__version__ = "0.1.0"
-
-from polus.tabular.package1.package2.awesome_function.awesome_function import (  # noqa # pylint: disable=unused-import
-    awesome_function,
-)
