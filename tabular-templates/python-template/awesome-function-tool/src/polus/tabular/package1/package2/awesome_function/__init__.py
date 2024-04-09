27995c9d73c40ff94bd508ba072e8791f06be4f8
hamshkhawar <hamdahshafqat@hotmail.com>
2024-04-09 14:05:52 -0500

    added template for tabular tools

---
+++ tabular-templates/python-template/awesome-function-tool/src/polus/tabular/package1/package2/awesome_function/__init__.py
@@ -0,0 +1,7 @@
+"""awesome_function."""
+
+__version__ = "0.1.0"
+
+from polus.tabular.package1.package2.awesome_function.awesome_function import (  # noqa # pylint: disable=unused-import
+    awesome_function,
+)
---
+++ tabular-templates/python-template/awesome-function-tool/src/polus/tabular/package1/package2/awesome_function/awesome_function.py
@@ -0,0 +1,16 @@
+"""Awesome Tool."""
+
+from pathlib import Path
+
+
+def awesome_function(inp_dir: Path, filepattern: str, out_dir: Path):
+    """Awesome Tool.
+
+    Args:
+        inp_dir: input directory to process
+        filepattern: filepattern to filter inputs
+        out_dir: output directory
+    Returns:
+        None
+    """
+    pass
